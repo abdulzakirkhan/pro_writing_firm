7eb15273a8cdc1673745607161d72567cacccfc8
Abdul Zakir <128758587+abdulzakirkhan@users.noreply.github.com>
2025-05-08 11:11:19 +0500

    download zip file batch

--- src/components/Order/Order.tsx
+++ src/components/Order/Order.tsx
@@ -1,20 +1,29 @@
+import toast from "react-hot-toast";
 import { FaDownload } from "react-icons/fa";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
-const Order = ({order}) => {
-  const orders=order?.orders
-  console.log("order :",orders)
+const Order = ({ order }) => {
+  const orders = order?.orders;
   const downloadFiles = orders[0]?.downloadFiles;
-  const downloadFilesLength =
-  Array.isArray(downloadFiles) 
-    ? downloadFiles.length 
-    : (!downloadFiles ? 1 : 0);
-
-
-  console.log("order :", order)
+  const downloadFilesLength = Array.isArray(downloadFiles)
+    ? downloadFiles.length
+    : !downloadFiles
+    ? 1
+    : 0;
+const d =orders[0]?.downloadFiles
+const nwfile=order.orders[0]?.downloadFile
+// console.log("order :", order.orders[0]?.downloadFile)
   return (
     <div
       className="bg-white rounded-xl flex flex-col gap-4 shadow px-5 pt-8 w-full h-[230px] relative overflow-hidden"
-      style={{ borderLeft: `4px solid ${order.paymentStatus === "paid" ?"#FDCC7E" : order.paymentStatus === "Partial Paid" ? "#D33316" : ""}` }}
+      style={{
+        borderLeft: `4px solid ${
+          order.paymentStatus === "paid"
+            ? "#FDCC7E"
+            : order.paymentStatus === "Partial Paid"
+            ? "#D33316"
+            : ""
+        }`,
+      }}
     >
       {/* Header Row */}
       <div>
@@ -23,10 +32,19 @@ const Order = ({order}) => {
             {order.batchNo}
           </h3>
           {/* <MdOutlineKeyboardArrowRight size={35} className="text-[#13A09D]" /> */}
-          <button className="bg-[#13A09D] text-white text-xs px-3 py-1.5 rounded flex items-center gap-2">
-          <FaDownload size={12} />
-          Files ({downloadFilesLength})
-        </button>
+          <button onClick={() => {
+            // Start download
+            const link = document.createElement("a");
+            link.href = nwfile;
+            console.log("downloadFiles",nwfile)
+            link.download = ""; // optional, you can put custom filename here e.g. "order-file.pdf"
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+          }} className="bg-[#13A09D] text-white text-xs px-3 py-1.5 rounded flex items-center gap-2">
+            <FaDownload size={12} />
+            Files ({downloadFilesLength})
+          </button>
         </div>
       </div>
       <div className="flex justify-between items-center gap-2">
@@ -38,15 +56,22 @@ const Order = ({order}) => {
             {order?.type} <span className="text-gray-500">({order?.qty})</span>
           </p>
         </div>
-        
+
         <div className="">
-            <p className="text-sm font-bold text-gray-600">
-              Total Price:{" "}
-              <span className="text-[#157BA7] px-1 font-semibold">${order.price}</span>
-            </p>
-            <p className="text-sm font-bold text-gray-600">
+          <p className="text-sm font-bold text-gray-600">
+            Total Price:{" "}
+            <span className="text-[#157BA7] px-1 font-semibold">
+              ${order.price}
+            </span>
+          </p>
+          <p className="text-sm font-bold text-gray-600">
             Status:{" "}
-            <span className="font-medium" style={{ color: order.paymentStatus === "paid" ? "#22C55E" : "#FFC107" }}>
+            <span
+              className="font-medium"
+              style={{
+                color: order.paymentStatus === "paid" ? "#22C55E" : "#FFC107",
+              }}
+            >
               {order.paymentStatus}
             </span>
           </p>
@@ -55,14 +80,17 @@ const Order = ({order}) => {
       <div className="flex justify-between ">
         <div className="">
           <p className="text-xs text-gray-600">
-            Order placed: <span className="text-gray-800">{order.orderPlacedDate}</span>
+            Order placed:{" "}
+            <span className="text-gray-800">{order.orderPlacedDate}</span>
           </p>
           <p className="text-xs text-gray-600">
             Deadline: <span className="text-gray-800">{order.deadline}</span>
           </p>
         </div>
         <div className="">
-          <button className="bg-[#137DA0] px-3 py-2 rounded-md text-white">Make Payment</button>
+          <button className="bg-[#137DA0] px-3 py-2 rounded-md text-white">
+            Make Payment
+          </button>
         </div>
       </div>
     </div>
